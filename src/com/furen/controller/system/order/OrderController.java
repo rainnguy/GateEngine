3eb78bb2603fa713f02efa220551988698d407cf
xk11223 <xk11223@126.com>
2016-06-24 09:53:30 +0800

    add user_code

--- src/com/furen/controller/system/order/OrderController.java
+++ src/com/furen/controller/system/order/OrderController.java
@@ -5,20 +5,25 @@ import java.util.Map;
 
 import javax.annotation.Resource;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.furen.controller.base.BaseController;
+import com.furen.entity.system.User;
 import com.furen.service.system.order.OrderService;
 import com.furen.util.AppUtil;
+import com.furen.util.Const;
 import com.furen.util.PageData;
 
 /**
  * 订单
  * 
  * @author _xk 2016-06-21
- * @version 2.0v
+ * @version 1.0v
  */
 @Controller
 @RequestMapping("/order/")
@@ -41,12 +46,18 @@ public class OrderController extends BaseController {
 		PageData pd = new PageData();
 		
 		pd = this.getPageData();
-		// TODO
+		
+		Subject subject = SecurityUtils.getSubject();  
+		Session session = subject .getSession();
+		User usera = (User)session.getAttribute(Const.SESSION_USER);
+
+		String userCode = usera.getUSERNAME();
+		
+		pd.put("userCode", userCode);
 		Integer count = 0;
 		try {
 			count = orderService.insertOrder(pd);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			errInfo = "error";
 			map.put("errInfo", errInfo);
