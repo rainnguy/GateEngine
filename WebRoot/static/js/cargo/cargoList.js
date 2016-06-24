4aede78a2d6373f523cbe25afaa7613063e70811
xk11223 <xk11223@126.com>
2016-06-24 14:27:54 +0800

    add js

---
+++ WebRoot/static/js/cargo/cargoList.js
@@ -0,0 +1,12 @@
+var locat = (window.location + '').split('/');
+$(function() {
+	if ('tool' == locat[3]) {
+		locat = locat[0] + '//' + locat[2];
+	} else {
+		locat = locat[0] + '//' + locat[2] + '/' + locat[3];
+	}
+	;
+});
+
+$(top.hangge());
+
